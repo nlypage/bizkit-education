de9d2c214a43d89790d26b7c8f228468995b8c19
qushedo <123452935+qushedo@users.noreply.github.com>
2024-07-02 17:13:04 +0300

    add: generateJWT and parseJWT

---
+++ backend/internal/domain/utils/jwt.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/nlypage/bizkit-education/internal/domain/common/errroz"
+	"os"
+	"strings"
+	"time"
+)
+
+var SecretKey = os.Getenv("JWT_SECRET")
+
+func GenerateJwt(uuid string, password string) (string, error) {
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer:    fmt.Sprintf("%s %s", uuid, password),
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+	})
+	return claims.SignedString([]byte(SecretKey))
+
+}
+
+func ParseJwt(cookie string) (string, string, error) {
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
+	})
+	if err != nil || !token.Valid {
+		return "", "", err
+	}
+	claims := token.Claims.(*jwt.StandardClaims)
+
+	issuerSlice := strings.Split(claims.Issuer, " ")
+	if len(issuerSlice) != 2 {
+		return "", "", errroz.InvalidIssuer
+	}
+	return issuerSlice[0], issuerSlice[1], nil
+}
