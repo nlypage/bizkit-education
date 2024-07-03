9f166bec26c5f0e5debb279112e787ccd2100835
Andrey Danilov <galagtik10@gmail.com>
2024-07-03 14:11:09 +0300

    feat: add adapters for question and answer

---
+++ backend/internal/adapters/database/postgres/question.go
@@ -0,0 +1,49 @@
+package postgres
+
+import (
+	"context"
+
+	"github.com/nlypage/bizkit-education/internal/domain/entities"
+	"gorm.io/gorm"
+)
+
+// questionStorage is a struct that contains a pointer to a gorm.DB instance.
+type questionStorage struct {
+	db *gorm.DB
+}
+
+// NewQuestionStorage is a function that returns a new instance of questionStorage.
+func NewQuestionStorage(db *gorm.DB) *questionStorage {
+	return &questionStorage{db: db}
+}
+
+// Create is a method to create a new Question in database.
+func (s *questionStorage) Create(ctx context.Context, question *entities.Question) (*entities.Question, error) {
+	err := s.db.WithContext(ctx).Create(&question).Error
+	return question, err
+}
+
+// GetByUUID is a method that returns an error and a pointer to a Question instance.
+func (s *questionStorage) GetByUUID(ctx context.Context, uuid string) (*entities.Question, error) {
+	var question *entities.Question
+	err := s.db.WithContext(ctx).Model(&entities.Question{}).Where("uuid = ?", uuid).First(&question).Error
+	return question, err
+}
+
+// GetAll is a method that returns a slice of pointers to Question instances.
+func (s *questionStorage) GetAll(ctx context.Context, limit, offset int) ([]*entities.Question, error) {
+	var question []*entities.Question
+	err := s.db.WithContext(ctx).Model(&entities.Question{}).Limit(limit).Offset(offset).Find(&question).Error
+	return question, err
+}
+
+// Update is a method to update an existing Question in database.
+func (s *questionStorage) Update(ctx context.Context, question *entities.Question) (*entities.Question, error) {
+	err := s.db.WithContext(ctx).Model(&entities.Question{}).Where("uuid = ?", question.UUID).Updates(&question).Error
+	return question, err
+}
+
+// Delete is a method to delete an existing Question in database.
+func (s *questionStorage) Delete(ctx context.Context, uuid string) error {
+	return s.db.WithContext(ctx).Unscoped().Delete(&entities.Question{}, "uuid = ?", uuid).Error
+}
