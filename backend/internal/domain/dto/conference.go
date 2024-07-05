b7f31add3000b59abcd41f9359d1592e38b7c0ec
qushedo <123452935+qushedo@users.noreply.github.com>
2024-07-05 11:25:52 +0300

    fix: time parsing

--- backend/internal/domain/dto/conference.go
+++ backend/internal/domain/dto/conference.go
@@ -5,6 +5,6 @@ import "time"
 type CreateConference struct {
 	Title       string    `json:"title" validate:"required,header"`
 	Description string    `json:"description" validate:"required,body"`
-	StartTime   time.Time `json:"-" validate:"required,datetime"`
+	StartTime   time.Time `json:"-"`
 	AuthorUUID  string    `json:"-" validate:"required,uuid4"`
 }
