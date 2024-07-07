dd844863e39c478f588359e734127d1bd006dab1
Andrey Danilov <galagtik10@gmail.com>
2024-07-08 02:37:55 +0300

    feat: add summarizer

---
+++ summarizer/wsgi.py
@@ -0,0 +1,8 @@
+from app import application
+import os
+
+if __name__ == "__main__":
+    application.config["YANDEX_COOKIE"] = os.environ["YANDEX_COOKIE"]
+    application.config["YANDEX_OAUTH"] = os.environ["YANDEX_OAUTH"]
+
+    application.run(ssl_context=('./certs/fullchain.pem', './certs/privkey.pem'))
