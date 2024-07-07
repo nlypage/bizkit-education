7d1cb83ba69d16f0d33ba56c6a1c0231ef467132
Andrey Danilov <galagtik10@gmail.com>
2024-07-08 02:58:59 +0300

    fix: remove listening ssl from summarizer

--- summarizer/wsgi.py
+++ summarizer/wsgi.py
@@ -5,4 +5,5 @@ if __name__ == "__main__":
     application.config["YANDEX_COOKIE"] = os.environ["YANDEX_COOKIE"]
     application.config["YANDEX_OAUTH"] = os.environ["YANDEX_OAUTH"]
 
-    application.run(ssl_context=('./certs/fullchain.pem', './certs/privkey.pem'))
+    # application.run(ssl_context=('./certs/fullchain.pem', './certs/privkey.pem'))
+    application.run()
