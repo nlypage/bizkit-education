944b25954cdabf3c808325a8063e9ac83e778a3b
qushedo <123452935+qushedo@users.noreply.github.com>
2024-07-08 21:33:49 +0300

    add: conference scheduler

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -4,12 +4,16 @@ import (
 	"github.com/nlypage/bizkit-education/cmd/app"
 	"github.com/nlypage/bizkit-education/internal/adapters/config"
 	"github.com/nlypage/bizkit-education/internal/adapters/controller/api/setup"
+	"github.com/nlypage/bizkit-education/internal/domain/scheduler"
 )
 
 // main is the entry point of the application.
 func main() {
 	appConfig := config.GetConfig()
 	bizkitEduApp := app.NewBizkitEduApp(appConfig)
+
+	conferenceScheduler := scheduler.NewConferenceScheduler(bizkitEduApp)
+	conferenceScheduler.Start()
 
 	setup.Setup(bizkitEduApp)
 	bizkitEduApp.Start()
---
+++ backend/internal/domain/scheduler/conference.go
@@ -0,0 +1,50 @@
+package scheduler
+
+import (
+	"github.com/nlypage/bizkit-education/cmd/app"
+	"github.com/nlypage/bizkit-education/internal/domain/entities"
+	"gorm.io/gorm"
+	"log"
+	"time"
+)
+
+// ConferenceScheduler is a struct that contains a pointer to a gorm.DB instance.
+type ConferenceScheduler struct {
+	db *gorm.DB
+}
+
+// NewConferenceScheduler is a function that returns a new instance of ConferenceScheduler.
+func NewConferenceScheduler(app *app.BizkitEduApp) *ConferenceScheduler {
+	return &ConferenceScheduler{db: app.DB}
+}
+
+// periodicallyArchiveConferences is a method that archives conferences every hour.
+func (s ConferenceScheduler) periodicallyArchiveConferences() {
+	ticker := time.NewTicker(1 * time.Hour)
+	for {
+		select {
+		case <-ticker.C:
+			s.archiveConferences()
+		}
+	}
+}
+
+// archiveConferences is a method that archives conferences that have already started.
+func (s ConferenceScheduler) archiveConferences() {
+	now := time.Now()
+	twoHoursAgo := now.Add(-2 * time.Hour)
+	result := s.db.Model(&entities.Conference{}).
+		Where("start_time <= ? AND archived = ?", twoHoursAgo, false).
+		Update("archived", true)
+
+	if result.Error != nil {
+		log.Printf("Error archiving conferences: %v\n", result.Error)
+	} else {
+		log.Printf("Successfully archived %v conferences\n", result.RowsAffected)
+	}
+}
+
+// Start is a method that starts the conference scheduler.
+func (s ConferenceScheduler) Start() {
+	go s.periodicallyArchiveConferences()
+}
