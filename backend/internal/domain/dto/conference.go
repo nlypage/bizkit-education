4b4dfec15d7814bc4edff5e7ac011d78bbede977
qushedo <123452935+qushedo@users.noreply.github.com>
2024-07-08 20:21:34 +0300

    fix: get my conferences add: start-time check

--- backend/internal/domain/dto/conference.go
+++ backend/internal/domain/dto/conference.go
@@ -6,7 +6,7 @@ import "time"
 type CreateConference struct {
 	Title       string    `json:"title" validate:"required,header"`
 	Description string    `json:"description" validate:"required,body"`
-	StartTime   time.Time `json:"-"`
+	StartTime   time.Time `json:"-" validate:"required"`
 	AuthorUUID  string    `json:"-" validate:"required,uuid4"`
 }
 
