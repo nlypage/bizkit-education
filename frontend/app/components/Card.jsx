fb38abec62ca07a336513da6c4322416edc54c4c
scarlettnik <swin-vu@ya.ru>
2024-07-10 01:55:23 +0300

    add reference to fix problem tommorow

--- frontend/app/components/Card.jsx
+++ frontend/app/components/Card.jsx
@@ -191,12 +191,52 @@
 // export default MapApp;
 
 
-import React from 'react'
+"use client";
+import React from "react";
+import dynamic from "next/dynamic";
 
-const Card = () => {
+// Dynamic import of react-leaflet components
+const MapContainer = dynamic(
+  () => import("react-leaflet").then((module) => module.MapContainer),
+  {
+    ssr: false, // Disable server-side rendering for this component
+  }
+);
+const TileLayer = dynamic(
+  () => import("react-leaflet").then((module) => module.TileLayer),
+  {
+    ssr: false,
+  }
+);
+const Marker = dynamic(
+  () => import("react-leaflet").then((module) => module.Marker),
+  {
+    ssr: false,
+  }
+);
+const Popup = dynamic(
+  () => import("react-leaflet").then((module) => module.Popup),
+  {
+    ssr: false,
+  }
+);
+
+const MapComponent = () => {
   return (
-    <div>Card</div>
-  )
-}
+    <MapContainer
+      center={[37.7577, -122.4376]}
+      zoom={8}
+      style={{ width: "100%", height: "400px" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={[37.7577, -122.4376]}>
+        <Popup>User Post Location</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
 
-export default Card
+export default MapComponent;
