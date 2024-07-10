b6fa1f59a6366aac528b8de3a3f39355b984f57e
scarlettnik <87085957+scarlettnik@users.noreply.github.com>
2024-07-10 12:25:16 +0300

    del logoWithTitle

--- frontend/app/components/forms/signupForm.jsx
+++ frontend/app/components/forms/signupForm.jsx
@@ -1,13 +1,13 @@
+import Title from "../base/Title"
 import styles from "../styles/SignupForm.module.css"
 import DefaultInput from "../ui/DefaultInput"
-import LogoWithTitle from "../ui/LogoWithTitle"
 import OpacitedButton from "../ui/OpacitedButton"
 import PurpleButton from "../ui/PurpleButton"
 
 export default function SignupForm() {
     return <div className={styles.login_form}>
         <div className={styles.welcome_message_box}>
-            <LogoWithTitle></LogoWithTitle>
+            <Title/>
             <p className={styles.welcome_message}>Вход в <span className={styles.welcome_message_title}>Biscuit</span></p>
         </div>
         <DefaultInput type={"text"} title={"Электронная почта"}></DefaultInput>
