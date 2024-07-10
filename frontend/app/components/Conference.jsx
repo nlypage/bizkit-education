452722b0c3628d1d4e24f339e063bbee63172f0a
scarlettnik <87085957+scarlettnik@users.noreply.github.com>
2024-07-10 11:08:54 +0300

    add youtube

--- frontend/app/components/Conference.jsx
+++ frontend/app/components/Conference.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
-const Conference = ({conference}) => {
+const Conference = ({ conference }) => {
+  console.log(conference)
+
   return (
-    <div>VeiwConference</div>
+    <div style={{ margin: '20vh' }}>
+      <iframe
+        width="560"
+        height="315"
+        src={`https://www.youtube.com/embed/${conference.url.split('/').pop()}`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+    </div>
   )
 }
 
