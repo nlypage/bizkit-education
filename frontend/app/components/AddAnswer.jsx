f08bb8f3d2a698b312a506aafc33907484e30514
Misha <toprobocopid@gmail.com>
2024-07-10 11:30:09 +0300

    Added question page

--- frontend/app/components/AddAnswer.jsx
+++ frontend/app/components/AddAnswer.jsx
@@ -2,6 +2,31 @@
 
 import React, { useState, useEffect } from "react";
 import { fetchWithAuth } from "../utils/api";
+import styles from './styles/AddAnswer.module.css'
+import answerQuestionStyles from './styles/QuestionPreview.module.css'
+import OpacitedButton from "./ui/opacitedButton";
+// {
+//   "uuid": "f413611a-198c-428a-b2c5-08f22595d5f3",
+//   "created_at": "2024-07-04T20:20:06.478291Z",
+//   "updated_at": "2024-07-04T20:20:06.478291Z",
+//   "author_uuid": "dc5edb8d-d889-4d96-8598-75e361066b6e",
+//   "header": "Текст",
+//   "body": "альтернативный текст",
+//   "subject": "Русский язык",
+//   "reward": 20,
+//   "closed": false,
+//   "answers": [
+//       {
+//           "uuid": "f1aa6a74-4fe9-4165-a2a5-bccacc476f8a",
+//           "created_at": "2024-07-04T20:24:48.354046Z",
+//           "updated_at": "2024-07-04T20:24:48.354046Z",
+//           "author_uuid": "dc5edb8d-d889-4d96-8598-75e361066b6e",
+//           "question_uuid": "f413611a-198c-428a-b2c5-08f22595d5f3",
+//           "body": "какой то текст",
+//           "is_correct": false
+//       }
+//   ]
+// }
 
 const AddAnswer = ({ question }) => {
   const [showAnswerInput, setShowAnswerInput] = useState(false);
@@ -74,24 +99,88 @@ const AddAnswer = ({ question }) => {
     }
   };
   return (
-    <div>
-      <div>Предмет: {data ? data.subject : "Loading..."}</div>
-      <div>Описание {data ? data.body : "Loading..."}</div>
-      <div>
-        {data
-          ? data?.answers?.map((answer) => (
-              <div key={answer.uuid}>
-                <div>{answer?.body}</div>
-                <button onClick={() => markAsCorrect(answer.uuid)}>
-                  Правильный ответ
-                </button>
-              </div>
-            ))
-          : "Еще нет ответов"}
+    <>
+      <div className={styles.question_box}>
+        <div className={answerQuestionStyles.question_preview_user_box}>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-YIGV8GTRHiW_KACLMhhi9fEq2T5BDQcEyA&s" alt="" className={answerQuestionStyles.question_preview_avatar}/>
+          <p className={answerQuestionStyles.question_preview_nickname}>{"Abob"}</p>
+          <hr className={answerQuestionStyles.question_preview_hr} style={{marginLeft:"0px"}}/>
+          <p className={answerQuestionStyles.question_preview_rank}>{"Megabrain"}</p>    
+          <hr className={answerQuestionStyles.question_preview_hr} style={{marginLeft:"0px"}}/>
+          <div className={answerQuestionStyles.question_preview_question_type} style={{marginTop: "20px", marginLeft:"0px"}}>
+
+            #Физика
+          </div>
+          <div className={answerQuestionStyles.question_preview_cost}  style={{marginTop: "17px", margin: "auto", float: "right", marginRight: "00px"}}>
+            150
+            <img src="biscuit.png" className={answerQuestionStyles.question_preview_cookie} alt="" />
+          </div>
+        </div>
+
+        <div className={styles.question_title}>{data ? data.subject : "Loading..."}</div>
+        <div className={styles.question_description}>{data ? data.body : "Loading..."}</div>
+        <div className={styles.question_bottombar}>
+          <div className={styles.question_bottombar_info}>
+            Вопрос <span style={{color: "#7950F2", fontWeight: "bold", marginLeft: "5px"}}> открыт</span>
+            <hr className={answerQuestionStyles.question_preview_hr} style={{marginLeft:"0px", marginTop: "10px"}}/>
+            <span style={{color: "#7950F2", fontWeight: "bold"}}>12 </span> <span style={{marginLeft: "5px"}}>ответов</span>
+          </div>
+          
+          <div className={styles.question_preview_answer_button_wrapper} style={{marginTop: "-30px",margin: "auto", marginRight: "10px", float: "right"}}>
+            <OpacitedButton onClick={() => answerClick(question.uuid)} title={"Ответить"}></OpacitedButton>
+          </div>  
+
+        </div>
       </div>
-      <button onClick={() => setShowAnswerInput(!showAnswerInput)}>
-        Ответить
-      </button>
+      
+      <div className={styles.question_answer_p}>
+        Ответы:
+      </div>
+
+      {/* DIV для примера */}
+      <div className={answerQuestionStyles.question_preview_box} key={answer.uuid} style={{marginTop: "30px"}}>
+        <div className={answerQuestionStyles.question_preview_user_box}>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-YIGV8GTRHiW_KACLMhhi9fEq2T5BDQcEyA&s" alt="" className={answerQuestionStyles.question_preview_avatar}/>
+                    <p className={answerQuestionStyles.question_preview_nickname}>{"Aboba"}</p>
+                    <hr className={answerQuestionStyles.question_preview_hr} />
+                    <p className={answerQuestionStyles.question_preview_rank}>{"Megabrain"}</p>    
+                  </div>
+                  <div className={styles.question_description} style={{marginTop: "20px"}}>
+                    fnsjafhjahfjalkjk ?fmkl kml
+                  </div>
+                  {/* ДОЛЖНА БЫТЬ КНОПКА ЗАКРЫТИЯ ВОПРОСА ЕСЛИ ЭТО ВОПРОС ОТ ПОЛЬЗОВАТЕЛЯ */}
+                  {/* <div className={answerQuestionStyles.question_preview_bottombar}>
+                    
+                    <div className={answerQuestionStyles.question_preview_answer_button_wrapper}>
+                      <OpacitedButton title={"Пометить как верный"}></OpacitedButton>
+                    </div>  
+                  </div> */}
+      </div>
+      <div>
+          {data
+            ? data?.answers?.map((answer) => (
+              <div className={answerQuestionStyles.question_preview_box} key={answer.uuid} style={{marginTop: "30px"}}>
+                <div className={answerQuestionStyles.question_preview_user_box}>
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-YIGV8GTRHiW_KACLMhhi9fEq2T5BDQcEyA&s" alt="" className={answerQuestionStyles.question_preview_avatar}/>
+                  <p className={answerQuestionStyles.question_preview_nickname}>{"Aboba"}</p>
+                  <hr className={answerQuestionStyles.question_preview_hr} />
+                  <p className={answerQuestionStyles.question_preview_rank}>{"Megabrain"}</p>    
+                </div>
+                <div className={styles.question_description} style={{marginTop: "20px"}}>
+                  fnsjafhjahfjalkjk ?fmkl kml
+                </div>
+                        {/* ДОЛЖНА БЫТЬ КНОПКА ЗАКРЫТИЯ ВОПРОСА ЕСЛИ ЭТО ВОПРОС ОТ ПОЛЬЗОВАТЕЛЯ */}
+                        {/* <div className={answerQuestionStyles.question_preview_bottombar}>
+                          
+                          <div className={answerQuestionStyles.question_preview_answer_button_wrapper}>
+                            <OpacitedButton title={"Пометить как верный"}></OpacitedButton>
+                          </div>  
+                        </div> */}
+              </div>
+              ))
+            : "Еще нет ответов"}
+      </div>
+      
       {showAnswerInput && (
         <div>
           <textarea
@@ -101,7 +190,10 @@ const AddAnswer = ({ question }) => {
           <button onClick={addAnswer}>Отправить</button>
         </div>
       )}
-    </div>
+
+      
+        
+    </>
   );
 };
 
