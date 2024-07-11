1abff3e3adef2e14181c8affcb9dc2c36726af6f
Andrey Danilov <galagtik10@gmail.com>
2024-07-11 11:23:21 +0300

    fix: update dto for event.go

--- backend/internal/domain/dto/event.go
+++ backend/internal/domain/dto/event.go
@@ -9,4 +9,15 @@ type CreateEvent struct {
 	AuthorUUID  string    `json:"author_uuid" validate:"required,uuid64"`
 	Longitude   float64   `json:"longitude" validate:"required,longitude"`
 	Latitude    float64   `json:"latitude" validate:"required,latitude"`
+	Address     string    `json:"address"`
+}
+
+type ReturnEvent struct {
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	StartTime   time.Time `json:"start_time"`
+	Longitude   float64   `json:"longitude"`
+	Latitude    float64   `json:"latitude"`
+	Address     string    `json:"address"`
+	Author      Author    `json:"author"`
 }
