c0ab71232e261cd32e8d4c96fa841db345aee9d3
Misha <toprobocopid@gmail.com>
2024-07-11 14:59:54 +0300

    Added maps design

--- frontend/app/components/ui/defaultInput.jsx
+++ frontend/app/components/ui/defaultInput.jsx
@@ -1,7 +1,7 @@
 import styles from "../styles/DefaultInput.module.css"
-export default function DefaultInput({title, type, onChange}) {
+export default function DefaultInput({title, type, onChange, name}) {
     return <div className={styles.input_box}>
         <p className={styles.input_title}>{title}</p>
-        <input onChange={onChange} className={styles.input} type={type} />
+        <input onChange={onChange} required className={styles.input} name={name} type={type} />
     </div>
 }
